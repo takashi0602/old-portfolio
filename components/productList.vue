920ce50b999ed088bee43c1449818e731ff5ade8
takashi <takashioicsn@gmail.com>
2019-04-27 01:43:21 +0900

    create product

---
+++ components/productList.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <div v-for="(item, index) in lists" :key="index">
+      <div class="c-product">
+        <div class="c-product__name">{{ item.name }}</div>
+        <div class="c-product__detail">{{ item.detail }}</div>
+        <div class="c-product__img c-image" :style="`background-image: url(${item.img})`"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    lists: {
+      type: Array,
+      default: []
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~/assets/scss/foundation/mixin.scss";
+@import "~/assets/scss/foundation/variable.scss";
+.c-product {
+  font-size: 0.8rem;
+  padding: 0 15px;
+  margin: 0 0 15px 0;
+  border-bottom: 3px dashed $green;
+  @include desktop() {
+    font-size: 1.2rem;
+  }
+  &__name {
+    margin-bottom: 10px;
+    &:before {
+      content: "";
+      display: inline-block;
+      width: 7px;
+      height: 7px;
+      background: $green;
+      margin-right: 5px;
+    }
+  }
+  &__detail {
+    padding-left: 12px;
+    margin-bottom: 15px;
+  }
+  &__img {
+    width: 75%;
+    height: 200px;
+    background-position: center center;
+    border: 5px solid $green;
+    cursor: pointer;
+    border-radius: 20px;
+    margin: 0 auto 20px auto;
+    @include desktop() {
+      width: 100%;
+      height: 400px;
+    }
+  }
+}
+</style>
